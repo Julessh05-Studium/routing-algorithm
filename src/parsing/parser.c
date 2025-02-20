5a901d9455dd7a7b235bd218e677f0df3dfbc8fc
Gregor Gottschewski <gregorgott.software@yahoo.com>
2025-02-20 10:06:44 +0100

    refactor: beautify code and adding doc comments
    add clang format

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -12,9 +12,28 @@
 
 #define BUFFER_SIZE 32
 
-int dist;
+int csv_column;
+int char_index;
+CITY *start_city;
+CITY *dest_city;
+FILE *map_file;
+DICTIONARY *dictionary;
 
-void init_csv_line() { dist = 0; }
+/**
+ * Initializes the global variables for parsing a new csv row.
+ */
+void init_csv_line() {
+  csv_column = 0;
+  char_index = 0;
+}
+
+/**
+ * Resets column counter and character counter for reading next line.
+ */
+void next_row() {
+  csv_column++;
+  char_index = 0;
+}
 
 /**
  * Adds the given start and destination with the given distance to the given
@@ -47,8 +66,7 @@ bool add_wp_to_dict(DICTIONARY *dictionary, CITY *start_city,
 
   add(val,
       (WAYPOINT){
-        .city = *start_city, .destination = *dest_city, .distance = dist
-      });
+          .city = *start_city, .destination = *dest_city, .distance = dist});
   return true;
 }
 
@@ -60,96 +78,163 @@ void free_mem(FILE *file, char *val) {
   fclose(file);
 }
 
-DICTIONARY *parse(char path[]) {
-  FILE *file = fopen(path, "r");
-  if (file == NULL) {
-    fclose(file);
-    return NULL;
+/**
+ * Allocates memory for start and destination city.
+ *
+ * @return true if malloc was successful, false otherwise
+ */
+bool malloc_start_dest_city() {
+  start_city = malloc(sizeof(CITY));
+  if (start_city == NULL) {
+    return false;
   }
 
-  DICTIONARY *dictionary = malloc(sizeof(DICTIONARY));
+  dest_city = malloc(sizeof(CITY));
+  if (dest_city == NULL) {
+    free(start_city);
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Sets the name of the given city.
+ *
+ * @param name name of the city
+ * @param city city to set the name
+ * @return a bool if the name was set successfully
+ */
+bool set_city_name(const char *name, CITY *city) {
+  city->name = malloc(strlen(name) + 1);
+  if (city->name == NULL) {
+    return false;
+  }
+  strncpy(city->name, name, char_index);
+  city->name[strlen(name)] = '\0';
+  return true;
+}
+
+/**
+ * Converts given string to integer
+ * @param val string to convert
+ * @return the converted integer
+ */
+int get_distance_from_str(const char *val) {
+  char tmp[char_index + 1];
+  strncpy(tmp, val + '\0', char_index);
+  tmp[char_index] = '\0';
+  return (int) strtoumax(tmp, nullptr, 10);
+}
+
+/**
+ * Handles a CSV-column seperator character (here: `,` comma).
+ *
+ * @param current_word
+ * @return
+ */
+bool handle_csv_column(const char *current_word) {
+  switch (csv_column) {
+    case 0: {
+      set_city_name(current_word, start_city);
+      break;
+    }
+    case 1: {
+      set_city_name(current_word, dest_city);
+      break;
+    }
+    default: {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ * Initializes the file reader and dictionary.
+ *
+ * @param path path to file map file.
+ * @return a bool if opening file and allocating memory for dictionary succeeded.
+ */
+bool init_parser(char path[]) {
+  map_file = fopen(path, "r");
+  if (map_file == NULL) {
+    fclose(map_file);
+    return false;
+  }
+
+  dictionary = malloc(sizeof(DICTIONARY));
   if (dictionary == NULL) {
-    fclose(file);
-    return NULL;
+    fclose(map_file);
+    return false;
   }
 
-  int c;
-  char *val = malloc(32 * sizeof(char));
-  if (val == NULL) {
-    fclose(file);
+  return true;
+}
+
+/**
+ * Parses the map file given by the specified file path.
+ * After allocating needed memory and opening file stream,
+ * the file is parsed character by character until EOF is reached.
+ *
+ * @param path absolute path to the file
+ * @return a dictionary with data from the map.
+ */
+DICTIONARY *parse(char path[]) {
+  if (!init_parser(path)) {
+    return nullptr;
+  }
+
+  int current_char;
+  char *current_word = malloc(32 * sizeof(char));
+  if (current_word == NULL || !malloc_start_dest_city()) {
+    fclose(map_file);
     free(dictionary);
-    return NULL;
+    return nullptr;
   }
 
   int buf_size = BUFFER_SIZE;
-  int csv_column = 0;
   init_csv_line();
-  int i = 0;
 
-  CITY *start_city = malloc(sizeof(CITY));
-  CITY *dest_city = malloc(sizeof(CITY));
-
-  while ((c = getc(file)) != EOF) {
-    if (i == BUFFER_SIZE - 1) {
+  while ((current_char = getc(map_file)) != EOF) {
+    if (char_index == BUFFER_SIZE - 1) {
       buf_size += BUFFER_SIZE;
-      char *tmp = realloc(val, buf_size);
+      char *tmp = realloc(current_word, buf_size);
       if (tmp == NULL) {
-        free_mem(file, val);
-        return NULL;
+        free_mem(map_file, current_word);
+        return nullptr;
       }
-      val = tmp;
+      current_word = tmp;
     }
 
-    if (c == '\n') {
-      val[i] = '\0';
+    if (current_char == '\n') {
+      add_wp_to_dict(dictionary, start_city, dest_city, get_distance_from_str(current_word));
 
-      char tmp[i + 1];
-      strncpy(tmp, val, i);
-      tmp[i] = '\0';
-      dist = atoi(tmp);
-
-      add_wp_to_dict(dictionary, start_city, dest_city, dist);
-      start_city = malloc(sizeof(CITY));
-      dest_city = malloc(sizeof(CITY));
-
-      if (start_city == NULL || dest_city == NULL) {
-        free_mem(file, val);
-        return NULL;
+      if (start_city == NULL || dest_city == NULL || !malloc_start_dest_city()) {
+        free_mem(map_file, current_word);
+        return nullptr;
       }
 
       init_csv_line();
-      csv_column = 0;
-      i = 0;
       continue;
     }
 
-    if (c == ',') {
-      val[i] = '\0';
-      switch (csv_column) {
-        case 0: {
-          start_city->name = malloc(strlen(val) + 1);
-          strncpy(start_city->name, val, i);
-          start_city->name[strlen(val)] = '\0';
-          break;
-        }
-        case 1:
-          dest_city->name = malloc(strlen(val) + 1);
-          strncpy(dest_city->name, val, i);
-          dest_city->name[strlen(val)] = '\0';
-          break;
-        default:
-          free_mem(file, val);
-          return NULL;
+    if (current_char == ',') {
+      current_word[char_index] = '\0';
+
+      if (!handle_csv_column(current_word)) {
+        free_mem(map_file, current_word);
+        return nullptr;
       }
 
-      i = 0;
-      csv_column++;
+      next_row();
       continue;
     }
 
-    val[i++] = c;
+    current_word[char_index++] = (char) current_char;
   }
 
-  free_mem(file, val);
+  free_mem(map_file, current_word);
   return dictionary;
 }
